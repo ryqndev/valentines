ce7245e5c3d7bcc3179df5f407f3ab744f257e79
Ryan Yang <ryanqyang@gmail.com>
2024-02-11 15:02:10 -0800

    happy valentines day

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import clsx from "clsx";
+import cn from "./App.module.scss";
+import { useCallback, useState } from "react";
+import Swal from "sweetalert2";
 
 function App() {
-  return <div>App</div>;
+  const [hovered, setHovered] = useState(false);
+
+  const accepted = useCallback(() => {
+    Swal.fire("Awh, love you too!");
+  }, []);
+
+  return (
+    <div className={cn.container}>
+      <div className={cn.content}>
+        <h1>Will you be my Valentines?</h1>
+
+        <div className={cn.buttons}>
+          <button className={clsx(cn.button, cn.yes)} onClick={accepted}>
+            Yes
+          </button>
+          <button
+            onMouseOver={() => setHovered(true)}
+            onMouseOut={() => setHovered(false)}
+            className={clsx(cn.button, hovered && cn.disappear)}
+          >
+            No
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
